24550db349a0927946005661669ef6caa290d756
DanielNeves <dbenditoneves@gmail.com>
2021-09-20 19:29:20 -0300

    Add Typescript e ajustando página Home para tsx

--- myskills/src/pages/Home.tsx
+++ myskills/src/pages/Home.tsx
@@ -3,13 +3,23 @@ import { View, Text, StyleSheet, TextInput, Platform, FlatList } from "react-nat
 import { Button } from "../components/Button";
 import { SkillCard } from "../components/SkillCards";
 
+interface SkillData {
+  id: string;
+  name: string;
+}
+
 export function Home() {
   const [newSkill, setNewSkill] = useState('');
-  const [mySkills, setMySkills] = useState([]);
+  const [mySkills, setMySkills] = useState<SkillData[]>([]);
   const [greeting, setGretting] = useState('');
 
   function handleAddNewSkill() {
-    setMySkills(oldState => [...oldState, newSkill]);
+    const data = {
+      id: String(new Date().getTime()),
+      name: newSkill
+    }
+
+    setMySkills(oldState => [...oldState, data]);
   }
 
   useEffect(() => {
@@ -43,9 +53,9 @@ export function Home() {
       <FlatList 
         showsVerticalScrollIndicator={false}
         data={mySkills}
-        keyExtractor={item => item}
+        keyExtractor={item => item.id}
         renderItem={({ item }) => (
-          <SkillCard skill={item}/>
+          <SkillCard skill={item.name}/>
         )}
       />
 
